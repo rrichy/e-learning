2812ad67a2309efad3a223fc7cbc73a4d941019c
rrrasulrichy <rasulrichy.lumanggal.palanggalan@enrise-corp.co.jp>
2022-10-12 12:44:58 +0800

    Update DevAccountSeeder.php

--- server/database/seeders/DevAccountSeeder.php
+++ server/database/seeders/DevAccountSeeder.php
@@ -18,7 +18,7 @@ class DevAccountSeeder extends Seeder
     public function run()
     {
         $affiliations = Affiliation::with('departments')->get();
-        $department_users = [];
+        // $department_users = [];
         // Admin account
         User::factory()->create([
             'name' => 'Sample Admin',
@@ -37,7 +37,7 @@ class DevAccountSeeder extends Seeder
             'affiliation_id' => $affiliation->id,
             'remarks' => fake()->text(50),
         ]);
-        $this->pushDepartmentUser($department_users, $affiliation, $corporate);
+        $this->pushDepartmentUser($affiliation, $corporate);
 
         // Individual account
         $affiliation = $affiliations->random();
@@ -49,7 +49,7 @@ class DevAccountSeeder extends Seeder
             'affiliation_id' => $affiliation->id,
             'remarks' => fake()->text(50),
         ]);
-        $this->pushDepartmentUser($department_users, $affiliation, $individual);
+        $this->pushDepartmentUser($affiliation, $individual);
 
         // Trial account
         User::factory()->create([
@@ -63,28 +63,27 @@ class DevAccountSeeder extends Seeder
         // Seed users
         $users = User::factory(10)->create();
 
-        $users->each(function ($user) use (&$department_users, $affiliations) {
+        $users->each(function ($user) use (&$affiliations) {
             if (in_array($user->membership_type_id, [MembershipType::CORPORATE, MembershipType::INDIVIDUAL])) {
                 $affiliation = $affiliations->random();
                 $user->affiliation_id = $affiliation->id;
                 $user->remarks = fake()->text(50);
                 $user->save();
 
-                $this->pushDepartmentUser($department_users, $user->affiliation, $user);
+                $this->pushDepartmentUser($user->affiliation, $user);
             }
         });
 
-        DepartmentUser::insert($department_users);
+        // DepartmentUser::insert($department_users);
     }
 
-    private function pushDepartmentUser(array &$department_users, mixed $affiliation, mixed $user)
+    private function pushDepartmentUser(mixed $affiliation, mixed $user)
     {
         if ($affiliation->departments->count()) {
             $parent_department = $affiliation->departments->random();
+            $department_users = [];
 
-            $department_users[] = [
-                'department_id' => $parent_department->id,
-                'user_id' => $user->id,
+            $department_users[$parent_department->id] = [
                 'order' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -93,14 +92,14 @@ class DevAccountSeeder extends Seeder
             if ($parent_department->childDepartments->count()) {
                 $child_department = $parent_department->childDepartments->random();
 
-                $department_users[] = [
-                    'department_id' => $child_department->id,
-                    'user_id' => $user->id,
+                $department_users[$child_department->id] = [
                     'order' => 2,
                     'created_at' => now(),
                     'updated_at' => now(),
                 ];
             }
+
+            $user->departments()->sync($department_users);
         }
     }
 }
