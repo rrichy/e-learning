9dd5dc21fe6638ab60db3c044e079a5ea52cea6c
j-serencio <joshua.alonsagay.serencio@enrise-corp.co.jp>
2022-10-13 10:14:32 +0800

    [feature] - Feature #2081

--- client/src/components/pages/Contact.tsx
+++ client/src/components/pages/Contact.tsx
@@ -1,29 +1,32 @@
-import { Container, Paper, Typography } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, Grid, Paper, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
-import React from "react";
 import Button from "@/components/atoms/Button";
-import Footer from "@/components/molecules/Footer";
 import { FormContainer } from "react-hook-form-mui";
 import { TextField } from "../molecules/LabeledHookForms";
+import { useState } from "react";
+import Labeler from "@/components/molecules/Labeler";
 
 function Contact() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
   return (
     <Stack justifyContent="space-between">
-      <Container>
-        <Stack spacing={3} sx={{ p: 3 }}>
-          <Paper variant="softoutline" sx={{ p: 6 }}>
-            <Stack spacing={3}>
-              <Typography 
-                fontWeight="bold" 
-                variant="h6" 
-                pl={1} 
-                sx={{ borderLeft: '5px solid #00c2b2' }}
-              >
-                お問い合わせ
-              </Typography>
-              <FormContainer>
-                <Stack spacing={3} alignItems="center">
-                  <Typography>講座やシステムに関する疑問・ご質問は、こちらのフォームよりお問い合わせください。</Typography>
+      <Paper variant="outlined">
+        <Stack spacing={3}>
+          <Typography variant="sectiontitle2">お問い合わせ</Typography>
+          <FormContainer>
+            <Stack spacing={5} alignItems="center">
+              <Typography>講座やシステムに関する疑問・ご質問は、こちらのフォームよりお問い合わせください。</Typography>
+              <Grid container>
+                <Grid xs></Grid>
+                <Grid xs={8}>
                   <TextField
                     name="contact"
                     label="お問い合わせ内容"
@@ -31,18 +34,54 @@ function Contact() {
                     multiline
                     rows={8}
                     placeholder="自由記入欄"
-                    // sx={{
-                    //   maxWidth: 700,
-                    //   textAlign: "center"
-                    // }}
                   />
-                  <Button variant="contained" color="warning" sx={{ width: 200, p: 2, borderRadius: 7 }}>Confirmation{/* 力確認画面へ */}</Button>
-                </Stack>
-              </FormContainer>
+                </Grid>
+                <Grid xs></Grid>
+              </Grid>
+              <Button 
+                variant="contained" 
+                color="warning" 
+                sx={{ 
+                  width: 200, 
+                  p: 2, 
+                  borderRadius: 7 
+                }}
+                onClick={handleOpen}
+              >
+                Confirmation{/* 力確認画面へ */}
+              </Button>
             </Stack>
-          </Paper>
+          </FormContainer>
+
+          <Dialog open={open} maxWidth="md">
+            <DialogTitle sx={{ px: 0, pt: 0 }}>
+              <Typography variant="sectiontitle1">お問い合わせ内容確認</Typography>
+            </DialogTitle>
+            <DialogContent>
+              <Stack alignItems="center" p={3}>
+                <Typography>こちらの内容でよろしければ、送信ボタンをクリックしてお問い合わせください。</Typography>
+              </Stack>
+              <Stack p={3}>
+                <Labeler label="お問い合わせ内容" compact>
+                  <Typography>sulod sa textarea</Typography>
+                </Labeler>
+              </Stack>
+              <Stack alignItems="center" spacing={2} p={3} direction="row" justifyContent="center">
+                <Button 
+                  color="secondary" 
+                  variant="contained" 
+                  rounded 
+                  large 
+                  type="button"
+                  onClick={handleClose}
+                >
+                  送信
+                </Button>
+              </Stack>
+            </DialogContent>
+          </Dialog>
         </Stack>
-      </Container>
+      </Paper>
     </Stack>
   );
 }
--- client/src/components/pages/Inquiries.tsx
+++ client/src/components/pages/Inquiries.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { Paper, Stack, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import MaterialTable from "material-table";
+import Link from "@/components/atoms/Link";
 
 function Inquiries() {
+  const [data, setData] = useState([
+    { 
+      name: "Trevion Shields", 
+      email: "trevionshields@gmail.com", 
+      start_date: "2022-09-20",
+      progress_rate: 20,
+      score: 8,
+      lecture_day: "2022-09-18"
+    },
+  ]);
+  
   return (
-    <div>Inquiries</div>
+    <Stack justifyContent="space-between">
+      <Paper variant="outlined">
+        <Stack spacing={3}>
+          <Typography variant="sectiontitle2">Inquiries</Typography>
+          <MaterialTable 
+            columns={[
+              { 
+                field: "name", 
+                title: "氏名",
+                render: (row) => (
+                  <Link to={`#`}>
+                    {row.name}
+                  </Link>
+                ) 
+              },
+              { field: "email", title: "メールアドレス" },
+              { field: "start_date", title: "受講開始日" },
+              { field: "progress_rate", title: "進捗率" },
+              { field: "score", title: "点数" },
+              { field: "lecture_day", title: "受講完了日" },
+            ]}
+            options={{
+              toolbar: false,
+              draggable: false,
+              paging: false,
+              maxBodyHeight: 600,
+            }}
+            components={{
+              Container: (props) => <Paper {...props} variant="table" />,
+            }}
+            data={data}
+          />
+        </Stack>
+      </Paper>
+    </Stack>
   )
 }
 
