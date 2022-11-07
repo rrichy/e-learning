3c07de15b580a2f31c6063517f78fb583e1e59c8
rrrasulrichy <rasulrichy.lumanggal.palanggalan@enrise-corp.co.jp>
2022-11-07 12:21:25 +0800

    Update api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -28,63 +28,93 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-    Route::get('/me', [AuthenticatedSessionController::class, 'show']);
-    Route::put('/me', [AuthenticatedSessionController::class, 'update']);
-    Route::get('/upload', [AuthenticatedSessionController::class, 'upload']);
     Route::post('/upload', [AuthenticatedSessionController::class, 'upload']);
+    Route::get('/me', [AuthenticatedSessionController::class, 'show']);
+    Route::get('/upload', [AuthenticatedSessionController::class, 'upload']);
     Route::get('/video', [AuthenticatedSessionController::class, 'video']);
+    Route::put('/me', [AuthenticatedSessionController::class, 'update']);
+    Route::put('/video/{video}/update', [AuthenticatedSessionController::class, 'updatePlayback'])->middleware('membership:individual');
+
     Route::get('/options', [OptionsController::class, 'index']);
 
-    Route::group(['middleware' => ['membership:admin,corporate,individual']], function () {
-        Route::get('/notice', [NoticeController::class, 'index']);
-        Route::get('/notice/{notice}', [NoticeController::class, 'show']);
-        
-        Route::group(['middleware' => ['membership:admin,corporate']], function () {
-            Route::get('/affiliation', [AffiliationController::class, 'index']);
-    
-            Route::delete('/account/{ids}', [AccountController::class, 'massDelete']);
-            Route::resource('/account', AccountController::class)->except(['create', 'edit', 'destroy']);
-    
-            Route::resource('/department', DepartmentController::class)->except(['create', 'edit', 'show']);
-    
-            Route::post('/category/{category}/duplicate', [CategoryController::class, 'duplicate']);
-            Route::delete('/category/{ids}', [CategoryController::class, 'massDelete']);
-            Route::resource('/category', CategoryController::class)->only(['index', 'store', 'update']);
-    
-            Route::put('/mail-template/mass', [MailTemplateController::class, 'massUpdate']);
-            Route::delete('/mail-template/{ids}', [MailTemplateController::class, 'massDelete']);
-            Route::resource('/mail-template', MailTemplateController::class)->only(['index', 'store', 'update']);
-    
-            Route::put('/course/mass', [CourseController::class, 'massUpdate']);
-            Route::put('/course/toggle', [CourseController::class, 'toggleStatus']);
-            Route::delete('/course/{ids}', [CourseController::class, 'massDelete']);
-            Route::get('/course/{course}/attendees', [CourseController::class, 'attendees']);
-            Route::resource('/course', CourseController::class)->only(['store', 'update']);
-    
-            Route::delete('/notice/{ids}', [NoticeController::class, 'massDelete']);
-            Route::resource('/notice', NoticeController::class)->only(['store', 'update']);
-        
-            Route::apiResource('inquiries', InquiryController::class);
+    Route::middleware('membership:admin,corporate,individual')->group(function () {
+
+        Route::middleware('membership:admin,corporate')->group(function () {
+
+            Route::prefix('/account')->group(function () {
+                Route::post('/', [AccountController::class, 'store']);
+                Route::get('/{account}', [AccountController::class, 'show']);
+                Route::get('/', [AccountController::class, 'index']);
+                Route::put('/{account}', [AccountController::class, 'update']);
+                Route::delete('/{ids}', [AccountController::class, 'massDelete']);
+            });
+
+            Route::prefix('/department')->group(function () {
+                Route::post('/', [DepartmentController::class, 'store']);
+                Route::get('/', [DepartmentController::class, 'index']);
+                Route::put('/{department}', [DepartmentController::class, 'update']);
+                Route::delete('/{department}', [DepartmentController::class, 'destroy']);
+            });
+
+            Route::prefix('/category')->group(function () {
+                Route::post('/{category}/duplicate', [CategoryController::class, 'duplicate']);
+                Route::post('/', [CategoryController::class, 'store']);
+                Route::get('/', [CategoryController::class, 'index']);
+                Route::put('/{category}', [CategoryController::class, 'update']);
+                Route::delete('/{ids}', [CategoryController::class, 'massDelete']);
+            });
+
+            Route::prefix('/mail-template')->group(function () {
+                Route::post('/', [MailTemplateController::class, 'store']);
+                Route::get('/', [MailTemplateController::class, 'index']);
+                Route::put('/mass', [MailTemplateController::class, 'massUpdate']);
+                Route::put('/{mail_template}', [MailTemplateController::class, 'update']);
+                Route::delete('/{ids}', [MailTemplateController::class, 'massDelete']);
+            });
+
+            Route::prefix('/affiliation')->group(function () {
+                Route::post('/', [AffiliationController::class, 'store'])->middleware('membership:admin');
+                Route::get('/', [AffiliationController::class, 'index']);
+                Route::put('/{affiliation}', [AffiliationController::class, 'update'])->middleware('membership:admin');
+                Route::delete('/{affiliation}', [AffiliationController::class, 'destroy'])->middleware('membership:admin');
+            });
         });
 
-        Route::get('/course', [CourseController::class, 'index']);
-        Route::get('/course/{course}', [CourseController::class, 'show']);
-    });
+        Route::prefix('/course')->group(function () {
+            Route::post('/', [CourseController::class, 'store'])->middleware('membership:admin,corporate');
+            Route::get('/{course}/attendees', [CourseController::class, 'attendees'])->middleware('membership:admin,corporate');
+            Route::get('/{course}', [CourseController::class, 'show']);
+            Route::get('/', [CourseController::class, 'index']);
+            Route::put('/mass', [CourseController::class, 'massUpdate'])->middleware('membership:admin,corporate');
+            Route::put('/toggle', [CourseController::class, 'toggleStatus'])->middleware('membership:admin,corporate');
+            Route::put('/{course}', [CourseController::class, 'update'])->middleware('membership:admin,corporate');
+            Route::delete('/{ids}', [CourseController::class, 'massDelete'])->middleware('membership:admin,corporate');
+        });
 
-    Route::group(['middleware' => ['membership:individual']], function () {
-        Route::get('/chapter/{chapter}/test', [ChapterController::class, 'showTest']);
-        Route::get('/chapter/{chapter}/proceed-test', [ChapterController::class, 'proceedTest']);
-        Route::post('/chapter/{chapter}/submit-test', [ChapterController::class, 'submitTest']);
-        Route::get('/chapter/{chapter}/lecture', [ChapterController::class, 'listVideos']);
-        Route::put('/video/{video}/update', [AuthenticatedSessionController::class, 'updatePlayback']);
-    // Route::resource('/chapter', ChapterController::class);
-    });
-    
-    Route::group(['middleware' => ['membership:admin']], function () {
-        Route::resource('/affiliation', AffiliationController::class)->only(['store', 'update', 'destroy']);
+        Route::prefix('/notice')->group(function () {
+            Route::post('/', [NoticeController::class, 'store'])->middleware('membership:admin,corporate');
+            Route::get('/{notice}', [NoticeController::class, 'show']);
+            Route::get('/', [NoticeController::class, 'index']);
+            Route::put('/{notice}', [NoticeController::class, 'update'])->middleware('membership:admin,coporate');
+            Route::delete('/{ids}', [NoticeController::class, 'massDelete'])->middleware('membership:admin,corporate');
+        });
 
-        Route::delete('/signature/{ids}', [SignatureController::class, 'massDelete']);
-        Route::resource('/signature', SignatureController::class)->only(['index', 'store', 'update']);
+
+        Route::get('/inquiries', [InquiryController::class, 'index'])->middleware('membership:admin,corporate');
+
+        Route::prefix('/chapter')->middleware('membership:individual')->group(function () {
+            Route::post('/{chapter}/submit-test', [ChapterController::class, 'submitTest']);
+            Route::get('/{chapter}/lecture', [ChapterController::class, 'listVideos']);
+            Route::get('/{chapter}/proceed-test', [ChapterController::class, 'proceedTest']);
+            Route::get('/{chapter}/test', [ChapterController::class, 'showTest']);
+        });
+
+        Route::prefix('/signature')->middleware('membership:admin')->group(function () {
+            Route::post('/', [SignatureController::class, 'store']);
+            Route::get('/', [SignatureController::class, 'index']);
+            Route::put('/{signature}', [SignatureController::class, 'update']);
+            Route::delete('/{ids}', [SignatureController::class, 'massDelete']);
+        });
     });
 });
 
