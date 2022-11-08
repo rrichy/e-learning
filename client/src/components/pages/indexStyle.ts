293d5589e4d245d89a483337274cd02857f67fc5
rrrasulrichy <rasulrichy.lumanggal.palanggalan@enrise-corp.co.jp>
2022-11-08 13:08:02 +0800

    [fix] overflow on main container

--- client/src/components/pages/indexStyle.ts
+++ client/src/components/pages/indexStyle.ts
@@ -2,7 +2,7 @@ import { SxProps, Theme } from "@mui/material";
 
 export default {
   py: { xs: 2, sm: 3 },
-  flex: 1,
+  maxWidth: { md: "calc(100vw - 257px)" },
   "& .MuiDataGrid-columnHeaders": {
     bgcolor: "common.black",
     "& .MuiCheckbox-root:not(.Mui-checked)": {
