a75ae4c59d44e4d2594de71c1dbaeaeebcaf42cb
rrrasulrichy <rasulrichy.lumanggal.palanggalan@enrise-corp.co.jp>
2022-12-01 17:22:51 +0800

    Update MyTable.tsx

--- client/src/components/atoms/MyTable.tsx
+++ client/src/components/atoms/MyTable.tsx
@@ -198,7 +198,7 @@ function MyTable<T extends unknown>({
           {table.getRowModel().rows.length === 0 && (
             <TableBody>
               <TableRow>
-                <TableCell colSpan={table.getAllColumns().length}>
+                <TableCell colSpan={table.getAllColumns().length + (onDragEnd ? 1 : 0)}>
                   <Box
                     pt={2}
                     width={1}
