00e6d28a26d84d769cccba681c02d0cd1ae9b0db
rrichy <rasulrichy@gmail.com>
2023-01-22 20:59:20 +0800

    [fix] snackbaralert lint error bug

--- client_vue/src/components/SnackbarAlert.vue
+++ client_vue/src/components/SnackbarAlert.vue
@@ -1,14 +1,16 @@
-<script setup lang="ts">
-import { useAlertStore } from "@/stores/alert";
-
-const alertProps = useAlertStore();
-</script>
-
+<!-- eslint-disable prettier-vue/prettier -->
 <template>
   <v-snackbar v-model="alertProps.show" :timeout="alertProps.timeout" :color="alertProps.color">
     {{ alertProps.text }}
   </v-snackbar>
 </template>
+
+<script setup lang="ts">
+import { useAlertStore } from "@/stores/alert";
+
+const alertProps = useAlertStore();
+// eslint-disable-next-line prettier-vue/prettier
+</script>
 
 <style scope>
 </style>
